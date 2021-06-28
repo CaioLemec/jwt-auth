d050f497dd2cc16c4f066ff4b7b7097d38f05258
Caio Lemec <caio.lemec@gmail.com>
2021-06-28 16:30:52 -0300

    salvando dados do usuario

---
+++ auth-front/src/pages/authneeded.tsx
@@ -0,0 +1,14 @@
+import { useContext } from "react";
+import { AuthContext } from "../contexts/AuthContext";
+import styles from './home.module.scss'
+
+export default function Authneeded() {
+    const { user } = useContext(AuthContext);
+    return (
+        <div className={styles.container}>
+        <h1>Usuário só acessa com autenticação bem sucedida.</h1>
+        <h2>{user.email}</h2>
+        <h2>{user.roles}</h2>
+        </div>
+    );
+}
